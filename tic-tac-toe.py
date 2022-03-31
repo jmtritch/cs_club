5202227e2ac0efb41b923ffe9a1d45733ac45371
J. Michael Tritchler <jmtritch@gmail.com>
2022-03-30 21:06:24 -0700

    Updated tic-tac-toe class to follow pycodestyle.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,8 +6,7 @@ class TicTacToe():
     :param str o_rep: Optional string representation of the second player
     '''
 
-
-    def __init__(self, x_rep: str='X', o_rep: str='O'):
+    def __init__(self, x_rep: str = 'X', o_rep: str = 'O'):
         '''
         Initialize the Tic-Tac-Toe Object.
         '''
@@ -19,7 +18,6 @@ class TicTacToe():
         # Initialize a boolean to store the current player's turn
         self.turn = False
 
-
     def check_winner(self) -> bool:
         '''
         Check the board for a winner
@@ -28,25 +26,25 @@ class TicTacToe():
         '''
 
         # Check rows
-        if self.board[0] == self.board[1] == self.board[2] != None:
+        if self.board[0] == self.board[1] == self.board[2] is not None:
             return True
-        if self.board[3] == self.board[4] == self.board[5] != None:
+        if self.board[3] == self.board[4] == self.board[5] is not None:
             return True
-        if self.board[6] == self.board[7] == self.board[8] != None:
+        if self.board[6] == self.board[7] == self.board[8] is not None:
             return True
 
         # Check columns
-        if self.board[0] == self.board[3] == self.board[6] != None:
+        if self.board[0] == self.board[3] == self.board[6] is not None:
             return True
-        if self.board[1] == self.board[4] == self.board[7] != None:
+        if self.board[1] == self.board[4] == self.board[7] is not None:
             return True
-        if self.board[2] == self.board[5] == self.board[8] != None:
+        if self.board[2] == self.board[5] == self.board[8] is not None:
             return True
 
         # Check diagonals
-        if self.board[0] == self.board[4] == self.board[8] != None:
+        if self.board[0] == self.board[4] == self.board[8] is not None:
             return True
-        if self.board[2] == self.board[4] == self.board[6] != None:
+        if self.board[2] == self.board[4] == self.board[6] is not None:
             return True
 
         return False
@@ -75,14 +73,13 @@ class TicTacToe():
         print('You will enter the number of the cell to select your play.')
         # Print the board with the cell numbers
         # Loop through the rows
-        for i in range(0,3):\
+        for i in range(0, 3):
             # Loop through the columns
-            for j in range(0,3):
+            for j in range(0, 3):
                 # Print the current cell
                 print('|', i*3+j+1, sep='', end='')
             print('|')
         print('')
-
 
     def show_board(self) -> None:
         '''
@@ -94,15 +91,14 @@ class TicTacToe():
         | | |X|
         '''
         # Print the current state of the board.
-        for i in range(0,3):
-            for j in range(0,3):
-                if self.board[i*3+j] == None:
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if self.board[i*3 + j] is None:
                     print('| ', end='')
                 else:
-                    print('|', self.board[i*3+j], sep='', end='')
+                    print('|', self.board[i*3 + j], sep='', end='')
             print('|')
         print('')
-
 
     def get_user_play(self) -> int:
         '''
@@ -125,15 +121,15 @@ class TicTacToe():
             # Check if the cell_id is between 1 and 9
             # and that the cell is empty
             if (len(cell_id) == 1
-                and cell_id >= '1'
-                and cell_id <= '9'
-                and self.board[int(cell_id)-1] == None):
+               and cell_id >= '1'
+               and cell_id <= '9'
+               and self.board[int(cell_id)-1] is None):
                 acceptable = True
             # Otherwise, update the prompt to let the user
             # know the cell ID is incorrect
             else:
-               prompt = ('Not a valid cell. '
-                         + 'Which cell would you like to select? (1-9): ')
+                prompt = ('Not a valid cell. '
+                          + 'Which cell would you like to select? (1-9): ')
 
         return int(cell_id)
 
@@ -142,7 +138,6 @@ class TicTacToe():
         Update the board with the current player's piece.
         '''
         self.board[cell_id-1] = self.reps[self.turn]
-
 
     def ask_for_new_game(self) -> bool:
         '''
@@ -161,7 +156,7 @@ class TicTacToe():
             yn = input(prompt)
             # Check if the answer is Y or N
             if (len(yn) == 1
-                and (yn.upper() == 'Y' or yn.upper() == 'N')):
+               and (yn.upper() == 'Y' or yn.upper() == 'N')):
                 acceptable = True
                 acceptable = True
             # Otherwise, update the prompt to let the user
@@ -176,7 +171,6 @@ class TicTacToe():
         # Otherwise return False
         return False
 
-
     def start_game(self) -> None:
         '''
         Start a new set of games.
@@ -185,10 +179,8 @@ class TicTacToe():
         # Initialize a new game state to track if the user wants to play a
         # game
         new_game = True
-
         # Keep playing a new game as long as the user desires
         while new_game:
-
             # Clear the board for a new game
             self.board = [None] * 9
             # Reset the current player's turn to the first player
@@ -197,13 +189,11 @@ class TicTacToe():
             win = False
             # Initialize the tie state to false
             tie = False
-
             # Show the instruction to the user
             self.show_instructions()
 
             # Keep playing until a player wins or the game ends in a tie
             while not win and not tie:
-
                 # Show the current state of the board
                 self.show_board()
                 # The the current player's play
@@ -227,7 +217,6 @@ class TicTacToe():
 
             # Ask if the player would like to play again
             new_game = self.ask_for_new_game()
-
 
 
 ttt = TicTacToe()
